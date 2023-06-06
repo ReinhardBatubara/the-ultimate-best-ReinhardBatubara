2767fb25576cd90985b2f9c4e3bd288705b2caeb
ReinhardBatubara <reinhardbatubara607@gmail.com>
2023-06-06 20:42:24 +0700

    Finish

---
+++ T03.java
@@ -0,0 +1,77 @@
+// 12S22012 - Reinhard Batubara
+// 12S22038 - Ade Yohana Azeka Siahaan
+import java.util.*;
+import java.lang.Math;
+
+public class T03 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judul, penulis;
+        int stok;
+        double hargapembelian, rating;
+        String penerbit, formatbuku, tahunterbit;
+        double minimummargin, penilaianbuku;
+        String kategoridiskon, kategoribuku, kategori;
+        
+        kategori = " ";
+        do {
+            isbn = input.nextLine();
+            if (isbn.equals("---")) {
+            } else {
+                judul = input.nextLine();
+                penulis = input.nextLine();
+                tahunterbit = input.nextLine();
+                penerbit = input.nextLine();
+                if (penerbit.equals("---")) {
+                    penerbit = "---";
+                } else {
+                    penerbit = penerbit;
+                }
+                formatbuku = input.nextLine();
+                hargapembelian = Double.parseDouble(input.nextLine());
+                minimummargin = Double.parseDouble(input.nextLine());
+                if (minimummargin > 0) {
+                    kategoridiskon = "---";
+                } else {
+                    if (minimummargin < -(hargapembelian * 40 / 100)) {
+                        kategoridiskon = "Once in a lifetime";
+                    } else {
+                        if (minimummargin < -(hargapembelian * 20 / 100)) {
+                            kategoridiskon = "Never come twice";
+                        } else {
+                            kategoridiskon = "No regret";
+                        }
+                    }
+                }
+                stok = Integer.parseInt(input.nextLine());
+                rating = Double.parseDouble(input.nextLine());
+                if (rating >= 4.7) {
+                    kategori = "Best Pick";
+                } else {
+                    if (rating >= 4.5) {
+                        kategori = "Must Read";
+                    } else {
+                        if (rating >= 4.0) {
+                            kategori = "Recommended";
+                        } else {
+                            if (rating >= 3.0) {
+                                kategori = "Average";
+                            } else {
+                                if (rating < 3.0) {
+                                    kategori = "Low";
+                                }
+                            }
+                        }
+                    }
+                }
+                if (kategoridiskon.equals("Once in a lifetime") && kategori.equals("Best Pick")) {
+                    kategoribuku = "The ultimate best";
+                } else {
+                    kategoribuku = "---";
+                }
+                System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + rating + "|" + kategori + "|" + kategoridiskon + "|" + kategoribuku);
+            }
+        } while (!isbn.equals("---"));
+    }
+}
